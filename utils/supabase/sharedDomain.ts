5533f8fb34d495c16a1d1ec7540934206695d75e
Jonathan Lovera <tech@rey.co>
2024-12-31 04:23:58 +1100

    console.logs

--- utils/supabase/sharedDomain.ts
+++ utils/supabase/sharedDomain.ts
@@ -1,13 +1,14 @@
 export const getSharedDomain = () => {
 	const url = process.env.NEXT_PUBLIC_URL;
-	console.log('getSharedDomain', url)
 	if (!url) {
 		throw new Error("NEXT_PUBLIC_URL is not defined");
 	}
+	console.log('NEXT_PUBLIC_URL', url)
 
 	try {
 		const parsedUrl = new URL(url);
 		const hostname = parsedUrl.hostname;
+		console.log('hostname', url)
 
 		// For localhost, return null or a default value
 		if (hostname === "localhost") {
@@ -20,6 +21,7 @@ export const getSharedDomain = () => {
 			// Handle subdomains like "auth.rey.co"
 			return `.${domainParts.slice(-2).join(".")}`;
 		}
+		console.log('domainParts', domainParts)
 
 		return `.${hostname}`;
 	} catch (error) {
