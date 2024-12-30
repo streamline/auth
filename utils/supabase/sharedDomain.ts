b5bbc7bf2c92a7ffeedd48673ae50af98828e8ec
Jonathan Lovera <tech@rey.co>
2024-12-31 04:30:13 +1100

    console.logs

--- utils/supabase/sharedDomain.ts
+++ utils/supabase/sharedDomain.ts
@@ -17,14 +17,16 @@ export const getSharedDomain = () => {
 
 		// For production domains, return the shared domain
 		const domainParts = hostname.split(".");
+		console.log('Domain Parts:', domainParts);
+
 		if (domainParts.length > 2) {
 			// Handle subdomains like "auth.rey.co"
 			return `.${domainParts.slice(-2).join(".")}`;
 		}
-		console.log('domainParts', domainParts)
 
 		return `.${hostname}`;
 	} catch (error) {
+		console.error("Error parsing URL:", error);
 		throw new Error(`Invalid URL in NEXT_PUBLIC_URL: ${url}`);
 	}
 };
